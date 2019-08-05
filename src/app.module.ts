1b2b7998237ce983ccb01973dde7bf2dc0b10591
Daniel Ayala <djdanielayalads99@gmail.com>
2019-08-05 13:36:48 -0500

    aplication

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/shopping-cart'),UsersModule, ProductsModule, OrdersModule ],
+  imports: [MongooseModule.forRoot('mongodb://localhost/shopping-cart'),UsersModule, ProductsModule, OrdersModule, AuthModule ],
   controllers: [AppController],
   providers: [AppService],
 })
